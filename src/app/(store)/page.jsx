650b7119e5a187511a2cb7d7a8059dc139bc3fa1
Zakaria <professeurzakaria5@gmail.com>
2026-02-06 21:59:39 +0100

    fixing and removing repeated Home page sliders

--- src/app/(store)/page.jsx
+++ src/app/(store)/page.jsx
@@ -1,18 +1,12 @@
 // My Components
 import StoreHomeCarocel from "@/components/StoreComponents/NavComp/StoreHomeCarocel";
-import StoreAllProducts from "@/components/StoreComponents/ProductComp/StoreAllProducts";
-import StoreNewProducts from "@/components/StoreComponents/ProductComp/StoreNewProducts";
-import StorePopularProducts from "@/components/StoreComponents/ProductComp/StorePopularProducts";
-import StoreProductsOnSale from "@/components/StoreComponents/ProductComp/StoreProductsOnSale";
+import StorePage from "@/components/StoreComponents/ProductComp/StorePage";
 
 export default async function Home() {
   return (
     <div className="mt-20 w-full">
       <StoreHomeCarocel />
-      <StoreNewProducts />
-      <StorePopularProducts />
-      <StoreProductsOnSale />
-      <StoreAllProducts />
+      <StorePage />
     </div>
   );
 }
